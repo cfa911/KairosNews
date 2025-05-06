ec87075b078606b8663926f35ad6df7af8b825aa
cfa911 <cfa911@outlook.pt>
2025-05-07 00:01:12 +0100

    Added apks of kairos news

--- kairos-news/app/_layout.tsx
+++ kairos-news/app/_layout.tsx
@@ -10,6 +10,7 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    Bahnschrift: require('../assets/fonts/Bahnschrift.ttf'),
   });
 
   if (!loaded) {
